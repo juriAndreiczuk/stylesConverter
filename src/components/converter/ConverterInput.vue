c67d5cbb786e4da7fa5f68df14d474150a5cc417
juriAndreiczuk <andreiczuk@tutanota.com>
2023-08-29 16:02:11 +0200

    converter files has been separated

---
+++ src/components/converter/ConverterInput.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import useConverterDivide from './../../use/converter/converterDivide'
+  const {inputCode, convertToScss} = useConverterDivide()
+</script>
+
+<template>
+  <div>
+    <h2 class="font-bold pb-1 text-2xl text-slate-200">Figma code</h2>
+    <textarea
+      v-model="inputCode"
+      ref="textArea"
+      class="h-72 w-full resize-none py-3 px-5 text-1xl border-2 text-slate-200 bg-slate-800 border-cyan-100 block"
+    >
+    </textarea>
+    <div>
+      <button
+        @click="convertToScss"
+        class="bg-cyan-900 p-3 text-1xl text-slate-100 mt-3"
+      >
+      Convert styles</button>
+    </div>
+  </div>
+</template>
