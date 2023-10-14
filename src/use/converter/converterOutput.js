2d497e30046dbba930f39ca781f7a4687818cb03
juriAndreiczuk <andreiczuk@tutanota.com>
2023-10-14 16:36:05 +0200

    textScss mixin has been created

--- src/use/converter/converterOutput.js
+++ src/use/converter/converterOutput.js
@@ -3,12 +3,28 @@ import { useConverterStore } from './../../stores/converter'
 
 const useConverterOutput = () => {
   const converterStore = useConverterStore()
-  const getKeyVal = (str) => {
-    return ({
-      key: str.slice(0, str.indexOf(':')),
-      val: str.slice(str.indexOf(':') + 1)
-    })
+  const getKeyVal = str => {
+    const [key, val] = str.split(':').map((item) => item.trim())
+    return { key, val }
   } 
+
+  const textScssMixin = computed(() => {
+    const allValues = converterStore.textArray.reduce((acc, item) => {
+      const { key, val } = getKeyVal(item)
+      acc[key] = val
+      return acc
+    }, {})
+
+    if(JSON.stringify(allValues) === '{}') return ''
+
+    const fontSize = allValues['font-size']?.slice(0, allValues['font-size'].indexOf('px')).trim() || '18'
+    const color = allValues['color'] || '$main-color'
+    const fontFamily = allValues['font-family'] || '$normal-font'
+    const lineHeight = allValues['line-height'] || '1.5'
+
+    return `@include page-text(${fontSize}, ${color}, ${fontFamily}, ${lineHeight});`
+  })
+
   const sizesScssMixin = computed(() => {
     return converterStore.sizeArray.map(item => {
       const { key, val } = getKeyVal(item)
@@ -25,6 +41,7 @@ const useConverterOutput = () => {
 
   const scss = computed(() => {
     return `
+      ${textScssMixin.value}
       ${sizesScssMixin.value.join('\n')}
       ${indentScssMixin.value.join('\n')}
     `
